68795e244155f6879e0b525e2aa0244607eba31a
Navy Gautam <141498497+GAUTAMNAVY@users.noreply.github.com>
2023-12-29 00:08:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function sendWhatsAppMessages() {
+    const message = document.getElementById('message').value;
+    const csvFile = document.getElementById('csvFile').files[0];
+
+    if (!message || !csvFile) {
+        alert('Please enter a message and select a CSV file.');
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append('message', message);
+    formData.append('csvFile', csvFile);
+
+    fetch('send_messages.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred while sending messages.');
+    });
+}
